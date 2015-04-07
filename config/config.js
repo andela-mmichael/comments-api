5b0dd715355f987b93bf3cf57a280dabb5480c51
mel <mel.michael@andela.co>
2015-04-07 13:25:04 +0100

    refactor code

---
+++ config/config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = (function(){
+  return {
+      port: process.env.PORT || 8100,
+      database: {
+          development: { uri: 'mongodb://localhost/commentsDB' },
+          production: { uri: process.env.MONGOLAB_URI }
+      }
+
+  };
+
+
+})();
