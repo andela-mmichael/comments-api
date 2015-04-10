84afa737a9e4249c4f3262e6fe0a504a65bba52e
mel <mel.michael@andela.co>
2015-04-10 18:54:34 +0100

    write unit test for model

--- tests/comments/comments.model.spec.js
+++ tests/comments/comments.model.spec.js
@@ -1,50 +1,40 @@
-// var app = require('../../server');
 var mongoose = require('mongoose');
-var should = require('should');
-// var CommentModel = mongoose.model('Comment');
 var CommentModel = require('../../app/comments/models/comments.model');
-// var config  = require('../../config/config');
+var config = require('../../config/config')();
+
+mongoose.connect(config.database[process.env.NODE_ENV].uri); //Connect to database
+
 var comment;  //Global variable
-  console.log("am here");
-//Connect to Database
-// var db = mongoose.connect(config.database[process.env.NODE_ENV].uri, function(err){
-//   if(err){
-//     // console.log("Couldn't connect to Database");
-//   }
-// });
 
 //Unit test
 describe('Comment Model Unit Test', function(){
-   console.log("got here");
-  describe('Save Method', function(){
-    beforeEach(function(done){
-      comment = new CommentModel({
-        name: 'Just any name',
-        content: 'What\'s on your mind',
-        createdOn: new Date()
-      });
+  beforeEach(function(){
+    comment = new CommentModel({
+      name: 'Just your name',
+      content: 'What\'s on your mind'
+    });
+    
+  });
+
+  afterEach(function(done){
+    CommentModel.remove({}, function(err){
+      if(err){
+        console.log(err);
+      }
+    });     
+    done();
+  });
+
+  it('should save a new collection', function(done){
+    comment.save(function(err){
+      if(err){
+        console.log(err);
+      }
+      expect(err).toBeNull();
       done();
-       console.log("created comment");
     });
 
-    afterEach(function(done){
-      CommentModel.remove(function(err){
-        if(err){
-          console.log(err);
-          console.log("checking");
-        }
-      });
-      done();
-    });
-
-    it('should save a new collection', function(done){
-      comment.save(function(err, task){
-        console.log("called");
-        expect(err).toBeNull();
-        done();
-      });
-    });
-      
   });
 
 });
+
